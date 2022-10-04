984ea7681563bfd5897452715ac7662576015819
Donny eikmans <91986647+Donderiaan@users.noreply.github.com>
2022-10-04 12:39:22 +0200

    Update app.js

--- opdracht_5.1/app.js
+++ opdracht_5.1/app.js
@@ -1,2 +1,4 @@
 document.getElementById("h1").style.color = "Red";
 document.getElementById("h1").innerText = "Ik ben klaar";
+document.getElementById("p").innerText = "p text";
+document.getElementById("p").style.fontsize = "3 Rem";
