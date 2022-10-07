579c643f3eb268a9dbebf16a2664698c16485ccf
Donny eikmans <91986647+Donderiaan@users.noreply.github.com>
2022-10-07 09:57:36 +0200

    Add files via upload

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,30 @@
+function calculate(x,y)
+{
+    let answer = x + y;
+    console.log(answer);
+
+}
+
+calculate(1, 12)
+
+
+
+function multiplyByFive(num)
+{
+    let answer = num * 5;
+    console.log(answer);
+}
+
+multiplyByFive(7, 5)
+
+function calculateMinutes(num2)
+{
+     let answer = num2 / 60 
+     console.log(answer)
+}
+
+calculateMinutes(3600, 60);
+
+let input1 = document.getElementById('input1')
+let input2 = document.getElementById('input2')
+
