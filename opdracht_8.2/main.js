a54c8c673dde66e3f6414107dabc5470db64932f
Donny eikmans <91986647+Donderiaan@users.noreply.github.com>
2022-11-06 21:29:21 +0100

    Add files via upload

---
+++ opdracht_8.2/main.js
@@ -0,0 +1,45 @@
+let output = document.getElementById("Tof")
+
+function plusCalculate() 
+{
+    let input1 = document.getElementById("input")
+    let input2 = document.getElementById("input_1")
+    let x = Number(input1.value);
+    let y = Number(input2.value);
+    output.innerText = x + y;
+
+    if (x < 0 || y < 0) output.innerText = "het getal is te laag"
+}
+
+function minusCalculate() 
+{
+    let input1 = document.getElementById("input")
+    let input2 = document.getElementById("input_1")
+    let x = Number(input1.value);
+    let y = Number(input2.value);
+    output.innerText = x - y;
+
+    if (x < 0 || y < 0) output.innerText = "het getal is te laag"
+}
+
+function timesCalculate() 
+{
+    let input1 = document.getElementById("input")
+    let input2 = document.getElementById("input_1")
+    let x = Number(input1.value);
+    let y = Number(input2.value);
+    output.innerText = x * y;
+
+    if (x < 0 || y < 0) output.innerText = " het getal is te laag"
+}
+
+function divideCalculate() 
+{
+    let input1 = document.getElementById("input")
+    let input2 = document.getElementById("input_1")
+    let x = Number(input1.value);
+    let y = Number(input2.value);
+    output.innerText = x / y;
+
+    if (x < 0 || y < 0) output.innerText = "het getal is te laag"
+}
